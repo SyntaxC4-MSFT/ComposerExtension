eef6a425ab55c8c100ed46d75d7451d8b0146647
Cory Fowler <cfowler@microsoft.com>
2014-10-17 20:37:08 -0700

    caching the .bat file it isn't generated properly on Websites

--- Content/install.cmd
+++ Content/install.cmd
@@ -1,15 +1,9 @@
 # Install Composer
 
-IF EXIST Composer (
-  rd /S /q Composer
-)
-
-mkdir Composer
 cd Composer
-curl -sS https://getcomposer.org/installer | php
 
-IF EXIST Composer.bat (
-  rm -f Composer.bat
+IF EXIST composer.phar (
+  rm -f composer.phar
 )
 
-echo @php "%~dp0composer.phar" %*>composer.bat
+curl -sS https://getcomposer.org/installer | php
