ca545a403fa4898dfdf9394b6adbfd7533e7115b
Cory Fowler <cfowler@microsoft.com>
2015-07-23 01:32:39 -0700

    updated applicationHost to reject requests to vendor dir

--- Content/install.cmd
+++ Content/install.cmd
@@ -6,4 +6,12 @@ IF EXIST composer.phar (
   rm -f composer.phar
 )
 
+IF NOT EXIST "%APPDATA%\Composer" (
+  mkdir "%APPDATA%\Composer"
+)
+
+IF NOT EXIST "%LOCALAPPDATA%\Composer" (
+  mkdir "%LOCALAPPDATA%\Composer"
+)
+
 php -r "readfile('https://getcomposer.org/installer');" | php
